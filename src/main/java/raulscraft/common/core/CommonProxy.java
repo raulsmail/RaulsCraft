7c107e0ee8f6e19634cf3b40e22e86cc6e97c07c
raulsmail <raulsmail00@gmail.com>
2014-05-18 22:08:55 +0200

    Common Proxy

--- src/main/java/raulscraft/common/core/CommonProxy.java
+++ src/main/java/raulscraft/common/core/CommonProxy.java
@@ -1,5 +1,6 @@
 package raulscraft.common.core;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import raulscraft.common.RaulsCraft;
 import raulscraft.common.block.BlockBlueStone;
 import raulscraft.common.creativetab.CreativeTabRaulsCraft;
@@ -13,5 +14,7 @@ public class CommonProxy {
         RaulsCraft.creativeTabBlocks = new CreativeTabRaulsCraft();
 
         RaulsCraft.blockBlueStone = (new BlockBlueStone()).setCreativeTab(RaulsCraft.creativeTabBlocks).setHardness(0.8F).setBlockName("blueStone");
+
+        GameRegistry.registerBlock(RaulsCraft.blockBlueStone, "blueStone");
     }
 }
