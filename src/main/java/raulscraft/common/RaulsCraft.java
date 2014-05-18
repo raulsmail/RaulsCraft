1c4a2a85e1dbdc9fc375fda013f753ba849cdb61
raulsmail <raulsmail00@gmail.com>
2014-05-18 20:50:44 +0200

    RaulsCraft

--- src/main/java/raulscraft/common/RaulsCraft.java
+++ src/main/java/raulscraft/common/RaulsCraft.java
@@ -2,6 +2,12 @@ package raulscraft.common;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import raulscraft.common.core.CommonProxy;
 
 /**
@@ -17,4 +23,28 @@ public class RaulsCraft {
 
     @SidedProxy(clientSide = "raulscraft.client.core.ClientProxy", serverSide = "raulscraft.common.core.CommonProxy")
     public static CommonProxy proxy;
+
+    private static final Logger logger = LogManager.getLogger("RaulsCraft");
+
+    @Mod.EventHandler
+    public void preLoad(FMLPreInitializationEvent event)
+    {
+        proxy.initMod();
+    }
+
+    @Mod.EventHandler
+    public void load(FMLInitializationEvent event)
+    {
+    }
+
+    @Mod.EventHandler
+    public void postLoad(FMLPostInitializationEvent event)
+    {
+    }
+
+    public static void console(String s, boolean warning)
+    {
+        StringBuilder sb = new StringBuilder();
+        logger.log(warning ? Level.WARN : Level.INFO, sb.append("[").append(version).append("-RaulsCraft] ").append(s).toString());
+    }
 }
